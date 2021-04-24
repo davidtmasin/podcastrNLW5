7898bba8ffedabd1380465b157ed7152c88a921f
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-24 13:01:38 -0300

    Aplicando o conceito de SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-export default function Home() {
-  return (
-    <h1>Index</h1>
+export default function Home(props) {
+    // console.log(props.episodes)
+    return (
+        <div>
+            <h1>Index</h1>
+            <p>{JSON.stringify(props.episodes)}</p>
+        </div>
   )
+}
+
+
+export async function getStaticProps() {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return{
+        props: {
+            episodes: data,
+        },
+        // Recebe um num em segundos de quanto em quanto tempo desejo gerar uma nova versão desta página
+        revalidate: 60 * 60 * 8,
+    }
+    
 }
