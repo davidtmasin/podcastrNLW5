258db35082b5e073de6d6091fce730a5f0addfd0
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-24 09:45:33 -0300

    Evoluindo o projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function  Header(){
+    return(
+        <header>
+            <img src="/logo.svg" alt="Podcastr"/>
+            <p>O melhor para você ouvir sempre</p>
+            <span>Sab, 24 Abril</span>
+        </header>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header';
+
+export default function Home() {
+  return (
+      <>
+        <h1>Fala, galera!</h1>
+        <Header />      
+      </>
+  )
+}
