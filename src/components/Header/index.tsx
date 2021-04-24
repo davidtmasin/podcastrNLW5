0ded4dbf3297f05f048786699ffbd55b49494650
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-24 10:08:01 -0300

    Usando a biblioteca date-fns para trazer a data atual e formatada para ptBR

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
+// importar as funções para formatação de data
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
 export function  Header(){
+
+    // Fazer com que sempre pegue a data atual
+    // EEEEEE -> Mostra as primeiras letras do dia da semana, exemplo, Seg
+    // d -> Mostra o número do dia
+    // MMMM -> Mostra o nome inteiro do mês
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM',{
+        locale:ptBR,
+    });
+
+
     return(
-        <header>
+        <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr"/>
+
             <p>O melhor para você ouvir sempre</p>
-            <span>Sab, 24 Abril</span>
+
+            <span>{currentDate}</span>
         </header>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import { Header } from '../components/Header';
 
 export default function Home() {
   return (
-      <>
-        <h1>Fala, galera!</h1>
-        <Header />      
-      </>
+    <Header />
   )
 }
