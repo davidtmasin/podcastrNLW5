a84977dd0670d7063acc28fceb7e9145adf6c910
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-25 10:03:04 -0300

    Finalizando a ideia do SSG Dinâmico e iniciando o uso de ContextAPI

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -6,8 +6,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRouter } from 'next/router';
-
+// import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
@@ -34,9 +33,34 @@ type EpisodeProps ={
 // Este método irá implementar a ideia de "SSG dinâmico", diferentemente da HOME que é única
 // a página dos episódios trazem diversos episódios diferentes
 export const getStaticPaths: GetStaticPaths = async () => {
+
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return{
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
+
     return{
-        paths: [],
-        fallback: 'blocking'
+        //O que estiver dentro de paths -> serão as páginas estáticas já preparadas
+        paths,
+
+        // client (browser)      -   next.js (node.js)     -    server (back-end)
+        // Quando ocorre o true  -   Quando o ocorre o blocking
+
+        // A ideia do fallback: 'blocking' é que as páginas sejam geradas de forma incremental, conforme for acessando
+        // incremental static regeneration
+        fallback: 'blocking' //Melhor opção para SEO
     }
 }
 
@@ -44,7 +68,14 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export default function Episode({ episode }: EpisodeProps){
     //Todo método que inicia com "use" é um reactHook e só pode ser utilizado dentro de componentes
     // Não funcionará dentro do getStaticProps
-    const router = useRouter(); 
+
+    // const router = useRouter(); 
+
+    // Como estaremos utilizando o fallback: 'blocking', não será usado
+
+    // if(router.isFallback){
+    //     return <p>Carregando...</p>
+    // }
 
     return(
         <div className={styles.episode}>
